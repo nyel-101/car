cc574607036152490fa9f2443456bce8f92d6df6
nyel-101 <dandevjr@gmail.com>
2025-03-01 22:42:32 +0000

    perf(worker): fix auth tokens

--- fury/src/components/helper.js
+++ fury/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d96984abc94044e8862f41627df55280 = () => {
-  return '2026-01-05T09:49:05Z';
+const node_9b769bb1eb4348cd98a3c263d1b50963 = () => {
+  return '2025-03-01T22:42:32Z';
 };
