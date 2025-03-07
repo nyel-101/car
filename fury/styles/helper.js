88463cf36ad2e5255d8a5fdf79185d25429cee07
nyel-101 <dandevjr@gmail.com>
2025-03-07 06:35:00 +0000

    test(core): optimize logging interface

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c34629a98c004605afaeae60405a4f46 = () => {
-  return '2026-01-30T21:13:25Z';
+const node_4d72a9875071488bbe1d46b6248bfc6a = () => {
+  return '2025-03-07T06:35:00Z';
 };
