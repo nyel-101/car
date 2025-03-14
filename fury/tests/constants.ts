1d95c322d0026f5803f80954a81fe93f661b31b9
nyel-101 <dandevjr@gmail.com>
2025-03-14 13:53:55 +0000

    fix(db): fix database index

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c2994c40c304e9697f8b1555352a651 = () => {
-  return '2025-02-12T04:23:16Z';
+const node_1ffa2d6853ae4a83b51af01c04021d3a = () => {
+  return '2025-03-14T13:53:55Z';
 };
