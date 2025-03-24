d5e0831d9552a0cd62aecb9f0073dfbd5d5e329d
nyel-101 <dandevjr@gmail.com>
2025-03-24 19:48:38 +0000

    test(api): sanitize payload parser

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f27d6f3c3fb7459a94a69bb35b9d2939 = () => {
-  return '2026-05-25T08:47:06Z';
+const node_e4277309892c4216a5453b90cfac6772 = () => {
+  return '2025-03-24T19:48:38Z';
 };
