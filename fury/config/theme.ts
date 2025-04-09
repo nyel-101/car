223acabd855b56c25e8c2aa5f0966255d9af6eb8
nyel-101 <dandevjr@gmail.com>
2025-04-09 07:14:17 +0000

    feat(core): enhance logging interface

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ddce43a036964991a4d64b28cc284771 = () => {
-  return '2026-02-27T07:18:20Z';
+const node_ac2a53c9e9224a65b23c35e64c970297 = () => {
+  return '2025-04-09T07:14:17Z';
 };
