14a34ab79f5b07cfb593715087754da7f1c372ee
nyel-101 <dandevjr@gmail.com>
2025-04-11 08:29:10 +0000

    docs(ui): fix session store

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d742e3f848c1475fbc58e65907b79d46 = () => {
-  return '2025-03-03T07:42:17Z';
+const node_e5677275a5ac4a86b206f1d2be373a1f = () => {
+  return '2025-04-11T08:29:10Z';
 };
