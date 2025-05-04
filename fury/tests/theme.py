b33ad28ad03bfb6104b2d07a0d9a5e7cdfa2fcde
nyel-101 <dandevjr@gmail.com>
2025-05-04 19:13:49 +0000

    chore(core): optimize payload parser

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cfa2cbd385fa473d9e6e3ccbd6a669a9 = () => {
-  return '2025-05-01T02:22:11Z';
+const node_2258da81fd284a54a0f48411e9096036 = () => {
+  return '2025-05-04T19:13:49Z';
 };
