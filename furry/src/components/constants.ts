f5217c0e7f49383a8b590f8632afd1066f80b6a4
nyel-101 <dandevjr@gmail.com>
2025-05-14 13:02:58 +0000

    fix(worker): integrate middleware routes

--- furry/src/components/constants.ts
+++ furry/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_47fb89c67e6749519a482927755b38b5 = () => {
-  return '2026-01-07T16:23:14Z';
+const node_10aeb3da5b15429d802b0a481902e689 = () => {
+  return '2025-05-14T13:02:58Z';
 };
