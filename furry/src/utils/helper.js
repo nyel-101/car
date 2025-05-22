5958f9669d523b5b83ce81693ae65d83a9b667e2
nyel-101 <dandevjr@gmail.com>
2025-05-22 19:35:27 +0000

    perf(core): enhance auth tokens

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6fb9ef0704924e758f2c7be58c3d2b6e = () => {
-  return '2025-03-24T14:09:50Z';
+const node_c1e22737da6b4d84ba5591ffaf18822a = () => {
+  return '2025-05-22T19:35:27Z';
 };
