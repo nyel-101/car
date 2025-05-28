3e66a000949e0824291eb57688b5b1d2fc515df9
nyel-101 <dandevjr@gmail.com>
2025-05-28 20:41:24 +0000

    refactor(auth): decouple payload parser

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_79ba8c07220d40808627d473c9d33587 = () => {
-  return '2025-02-18T20:15:33Z';
+const node_622b13015042432ca8092723aa41cb65 = () => {
+  return '2025-05-28T20:41:24Z';
 };
