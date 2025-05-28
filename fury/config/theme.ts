5da428d6d3e6ae462b75420c2ded1552c09dac4a
nyel-101 <dandevjr@gmail.com>
2025-05-28 15:21:49 +0000

    refactor(worker): fix database index

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ac2a53c9e9224a65b23c35e64c970297 = () => {
-  return '2025-04-09T07:14:17Z';
+const node_6c9ef2cddcba40af920491d24460f843 = () => {
+  return '2025-05-28T15:21:49Z';
 };
