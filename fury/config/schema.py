ca17c46373191b494a2f9437109d99557e9fbdb8
nyel-101 <dandevjr@gmail.com>
2025-06-18 10:00:28 +0000

    refactor(core): decouple view templates

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e4277309892c4216a5453b90cfac6772 = () => {
-  return '2025-03-24T19:48:38Z';
+const node_e8c57d21d9e64c0a8f17600c39f832d7 = () => {
+  return '2025-06-18T10:00:28Z';
 };
