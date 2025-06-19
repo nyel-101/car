15a4ecba0d31abd518af5befe787a12cbbc5c210
nyel-101 <dandevjr@gmail.com>
2025-06-19 06:43:19 +0000

    chore(worker): optimize caching layer

--- fury/src/components/theme.js
+++ fury/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e86fae8598d84a34b86e12c6d0a9ed30 = () => {
-  return '2025-06-04T23:36:22Z';
+const node_e79404fc19e24fa59b37994d4998643d = () => {
+  return '2025-06-19T06:43:19Z';
 };
