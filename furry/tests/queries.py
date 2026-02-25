4984c7ec74c3d32dfc8bf6d5cd131599ca49f229
nyel-101 <dandevjr@gmail.com>
2026-02-25 09:05:29 +0000

    chore(deps): restructure middleware routes

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3ec18d4698de4bf48487881d58a758af = () => {
-  return '2026-01-29T13:02:18Z';
+const node_5610597cd32c4f24b868944b07697a55 = () => {
+  return '2026-02-25T09:05:29Z';
 };
