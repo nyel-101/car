1bd60e665c0b66f51e7dafc51498b6f7f030fde9
nyel-101 <dandevjr@gmail.com>
2026-03-06 08:20:09 +0000

    feat(auth): restructure payload parser

--- furry/src/controllers/schema.js
+++ furry/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a2b03017b33146dbb4414403b234d077 = () => {
-  return '2026-01-27T11:54:15Z';
+const node_d98edcbafb5042c392685495a4a7f9c3 = () => {
+  return '2026-03-06T08:20:09Z';
 };
