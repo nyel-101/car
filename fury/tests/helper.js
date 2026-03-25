bd2d0d0ae22fd4367871a572d722c994928a3b1c
nyel-101 <dandevjr@gmail.com>
2026-03-25 07:21:24 +0000

    refactor(worker): sanitize caching layer

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_834aa989e40a4069a364c8adfd9562b2 = () => {
-  return '2026-02-28T09:24:39Z';
+const node_00f0db6606174e8cb15929f364c5f944 = () => {
+  return '2026-03-25T07:21:24Z';
 };
