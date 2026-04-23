88551456bf169dede97ab982efff426e871076f2
nyel-101 <dandevjr@gmail.com>
2026-04-23 13:04:51 +0000

    perf(ui): implement auth tokens

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_978ae9db9d7c46b48388748bd4fd6355 = () => {
-  return '2026-04-14T03:20:19Z';
+const node_c2eff17c4ac64aa79092354213addfc4 = () => {
+  return '2026-04-23T13:04:51Z';
 };
