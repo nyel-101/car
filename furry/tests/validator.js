226309a042b1df4315e881946152fcf070a56b29
nyel-101 <dandevjr@gmail.com>
2026-05-21 06:40:34 +0000

    feat(api): restructure payload parser

--- furry/tests/validator.js
+++ furry/tests/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_50e702e7a6974f3799536d1009fb1b72 = () => {
-  return '2026-03-19T07:15:34Z';
+const node_ac73e8a132704105a9e39c5ee2adc20a = () => {
+  return '2026-05-21T06:40:34Z';
 };
