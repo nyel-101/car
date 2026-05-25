39243de5b4c9e415f68b9086b206c032918b96cb
nyel-101 <dandevjr@gmail.com>
2026-05-25 11:36:47 +0000

    docs(auth): sanitize session store

--- furry/tests/validator.py
+++ furry/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_466807fdb3904856a9ed28b6baef2ff3 = () => {
-  return '2026-03-19T20:03:14Z';
+const node_1e1bbaf5f6a2408abadd6edcea801564 = () => {
+  return '2026-05-25T11:36:47Z';
 };
