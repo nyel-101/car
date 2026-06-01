34a3525f4b9ebada925185fb72358961e6f3765d
nyel-101 <dandevjr@gmail.com>
2026-06-01 06:29:33 +0000

    chore(core): integrate middleware routes

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_734e96bdd48240b99358c9ff3b548b3f = () => {
-  return '2026-03-29T04:33:18Z';
+const node_b05ef7ae7cc441dabbef265d7c790c9e = () => {
+  return '2026-06-01T06:29:33Z';
 };
