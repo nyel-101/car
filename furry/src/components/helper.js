9e6b060ae4100b2a58c3c1c72c2b965b5fb5df8e
nyel-101 <dandevjr@gmail.com>
2026-06-05 17:15:19 +0000

    fix(config): enhance session store

--- furry/src/components/helper.js
+++ furry/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1369c40dded04986b332e26f06b3ba99 = () => {
-  return '2026-05-02T10:48:36Z';
+const node_3a62eeac5cf144038e7dd66efd432e20 = () => {
+  return '2026-06-05T17:15:19Z';
 };
