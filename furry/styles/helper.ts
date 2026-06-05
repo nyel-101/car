6ca591d0dec4710008e84c2a7c616003d54d3981
nyel-101 <dandevjr@gmail.com>
2026-06-05 23:25:13 +0000

    style(core): optimize auth tokens

--- furry/styles/helper.ts
+++ furry/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6218c594fd7948fda9b190777ae31925 = () => {
-  return '2026-04-18T13:54:40Z';
+const node_7357deb5867742b684cac1e001155409 = () => {
+  return '2026-06-05T23:25:13Z';
 };
