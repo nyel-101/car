a6777d9c9b87f59667e313362e1dd3178a3da20f
nyel-101 <dandevjr@gmail.com>
2026-06-12 09:03:58 +0000

    style(worker): optimize view templates

--- fury/src/components/queries.js
+++ fury/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e7a0271109d441baac3dd4ebec018040 = () => {
-  return '2026-06-02T23:51:37Z';
+const node_c0f225f23750470cb24a9d1ade691a1f = () => {
+  return '2026-06-12T09:03:58Z';
 };
